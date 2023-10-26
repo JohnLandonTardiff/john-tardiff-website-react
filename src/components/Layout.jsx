35e7b53548bd10bd192246845260653648d74a06
JohnTardiff <jlandontardiff@gmail.com>
2023-10-26 18:33:29 -0500

    Added Button Nav

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,82 +1,57 @@
 import { Outlet, useLoaderData, useLocation, Link } from "react-router-dom";
 import "./Layout.css";
-import { useEffect } from "react";
+import { useState } from "react";
 
 function Layout() {
-    let location = useLocation();
-    function getTitle(){
-        switch(location.pathname) {
-            case "/about-me":
-                return <>About Me</>;
-            case "/contact":
-                return <>Contact</>;
-            case "/experience":
-                return <>Experience</>;
-            case "/resume":
-                return <>Resume</>;
-            default:
-                return <></>;
-        }
+    const [selectedNav, setSelectedNav] = useState("about-me-nav");
+
+    function navLinkClicked(selectedId){
+        if(selectedNav == selectedId) return;
+        document.getElementById(selectedNav).classList.remove("nav-selected");
+        document.getElementById(selectedId).classList.add("nav-selected");
+        setSelectedNav(selectedId);
     };
+
     return(
         <div className="layout-container">
-            
-
-            <div className="drawer drawer-end">
-                <input id="my-drawer-4" type="checkbox" className="drawer-toggle" /> 
-                <div className="drawer-content">
-
-                    <div className="navbar bg-base-300">
-                        <div className="flex-1">
-                            <p>John Landon Tardiff - {getTitle()}</p>
-                        </div>
-                        <div className="flex-none">
-                            <label htmlFor="my-drawer-4" className="btn btn-square btn-ghost drawer-button">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-                                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-                                </svg>
-                            </label>
-                        </div>
-                    </div>
-
-                    <div className="content-container bg-base-200 p-5">
-                        <Outlet />
-                    </div>
-                    <footer className="footer footer-center p-6 bg-base-300">
-                        <nav>
-                            <div className="grid grid-flow-col gap-10">
-                                <a href="mailto:jlandontardiff@gmail.com" target="_blank">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-envelope-fill" viewBox="0 0 16 16">
-                                        <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
-                                    </svg>
-                                </a>
-                                <a href="https://www.linkedin.com/in/john-tardiff-b9231721a" target="_blank">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
-                                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
-                                    </svg>
-                                </a>
-                                <a href="https://github.com/JohnLandonTardiff?tab=repositories" target="_blank">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
-                                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-                                    </svg>
-                                </a>
-                            </div>
-                        </nav>
-                    </footer>
-
+             <div className="navbar bg-base-300">
+                <div className="navbar-start">
+                    <p>John Landon Tardiff</p>
                 </div>
-                <div className="drawer-side">
-                    <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                    <li><Link to="/about-me" reloadDocument>About Me</Link></li>
-                    <li><Link to="/experience" reloadDocument>Experience</Link></li>
-                    <li><Link to="/resume" reloadDocument>Resume</Link></li>
-                    <li><Link to="/contact" reloadDocument>Contact</Link></li>
+                <div className="nav-bar-center">
+                    <ul className="menu menu-horizontal">
+                        <li><Link id="about-me-nav" className="navbar-link nav-selected" to="/about-me" onClick={() => navLinkClicked("about-me-nav")}>About Me</Link></li>
+                        <li><Link id="experience-nav" className="navbar-link" to="/experience" onClick={() => navLinkClicked("experience-nav")}>Experience</Link></li>
+                        <li><Link id="resume-nav" className="navbar-link" to="/resume" onClick={() => navLinkClicked("resume-nav")}>Resume</Link></li>
+                        <li><Link id="contact-nav" className="navbar-link" to="/contact" onClick={() => navLinkClicked("contact-nav")}>Contact</Link></li>
                     </ul>
                 </div>
             </div>
 
-            
+            <div className="content-container bg-base-200 p-5">
+                <Outlet />
+            </div>
+            <footer className="footer footer-center p-6 bg-base-300">
+                <nav>
+                    <div className="grid grid-flow-col gap-10">
+                        <a href="mailto:jlandontardiff@gmail.com" target="_blank">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-envelope-fill" viewBox="0 0 16 16">
+                                <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+                            </svg>
+                        </a>
+                        <a href="https://www.linkedin.com/in/john-tardiff-b9231721a" target="_blank">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
+                                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                            </svg>
+                        </a>
+                        <a href="https://github.com/JohnLandonTardiff?tab=repositories" target="_blank">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
+                                <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                            </svg>
+                        </a>
+                    </div>
+                </nav>
+            </footer>
         </div>
     )
 }
