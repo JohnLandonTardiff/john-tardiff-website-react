4eefb08acb8dd8817a8c6e0ad6a9d655af691aff
JohnTardiff <jlandontardiff@gmail.com>
2023-10-30 21:10:25 -0500

    Added Resume PDF Viewer

--- src/components/pages/resume/Resume.jsx
+++ src/components/pages/resume/Resume.jsx
@@ -1,8 +1,12 @@
+import ResumePDF from "../../../assets/Resume-John-Tardiff-Software.pdf";
+import { Document, Page } from "react-pdf";
+
 function Resume() {
+    
     return(
         <div className="card bg-base-100 content-card">
             <div className="card-body">
-                Resume
+                <object data={ResumePDF} className="h-full w-1/2 m-auto"/>
             </div>
         </div>
     )
