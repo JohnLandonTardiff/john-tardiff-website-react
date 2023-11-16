d74aa0bde2c3574b469b31c5674fb705e0947f88
JohnLandonTardiff <jlandontardiff@gmail.com>
2023-11-16 12:10:03 -0600

    Added captions and card to carousel

--- src/components/pages/about-me/AboutMe.jsx
+++ src/components/pages/about-me/AboutMe.jsx
@@ -11,34 +11,56 @@ function AboutMe() {
     return(
         <div className="card bg-base-100 content-card">
             <div className="card-body text-3xl indent-10">
-                    <div className="carousel h-1/2">
+                <div className="card h-3/4 bg-base-200 py-5">
+                    <div className="carousel">
                         <div id="pic1" className="carousel-item w-full justify-center">
-                            <figure className="w-1/4"><img src={Bass}/></figure>
+                            <figure className="flex-col">
+                                <img src={Bass} className="w-1/4"/>
+                                <p className="text-2xl">Picture of me playing my bass guitar</p>
+                            </figure>
                         </div>
                         <div id="pic2" className="carousel-item w-full justify-center">
-                            <figure className="w-1/2"><img src={Girlfriend}/></figure>
+                            <figure className="flex-col">
+                                <img src={Girlfriend} className="w-5/12"/>
+                                <p className="text-2xl">Picture of my girlfriend and I on a date</p>
+                            </figure>
                         </div>
                         <div id="pic3" className="carousel-item w-full justify-center">
-                            <figure className="w-1/4"><img src={Nationals1}/></figure>
+                            <figure className="flex-col">
+                                <img src={Nationals1} className="w-1/4"/>
+                                <p className="text-2xl">Picture of me with my awards from BPA nationals 2022</p>
+                            </figure>
                         </div>
                         <div id="pic4" className="carousel-item w-full justify-center">
-                            <figure className="w-1/2"><img src={Nationals2}/></figure>
+                            <figure className="flex-col">
+                                <img src={Nationals2} className="w-5/12"/>
+                                <p className="text-2xl">Picture of me recieving first place for C# Programming at BPA nationals 2023</p>
+                            </figure>
                         </div>
                         <div id="pic5" className="carousel-item w-full justify-center">
-                            <figure className="w-1/4"><img src={Meowth}/></figure>
+                            <figure className="flex-col">
+                                <img src={Meowth} className="w-1/4"/>
+                                <p className="text-2xl">Picture of my cat Meowth</p>
+                            </figure>
                         </div>
                         <div id="pic6" className="carousel-item w-full justify-center">
-                            <figure className="w-1/4"><img src={Ash}/></figure>
+                            <figure className="flex-col">
+                                <img src={Ash} className="w-1/4"/>
+                                <p className="text-2xl">Picture of my cat Ash</p>
+                            </figure>
                         </div>
                     </div>
                     <div className="flex justify-center w-full py-1 gap-2">
-                        <a href="#pic1" className="btn btn-xs">1</a>
-                        <a href="#pic2" className="btn btn-xs">2</a>
-                        <a href="#pic3" className="btn btn-xs">3</a>
-                        <a href="#pic4" className="btn btn-xs">4</a>
-                        <a href="#pic5" className="btn btn-xs">5</a>
-                        <a href="#pic6" className="btn btn-xs">6</a>
+                        <a href="#pic1" className="btn btn-xs bg-base-300">1</a>
+                        <a href="#pic2" className="btn btn-xs bg-base-300">2</a>
+                        <a href="#pic3" className="btn btn-xs bg-base-300">3</a>
+                        <a href="#pic4" className="btn btn-xs bg-base-300">4</a>
+                        <a href="#pic5" className="btn btn-xs bg-base-300">5</a>
+                        <a href="#pic6" className="btn btn-xs bg-base-300">6</a>
                     </div>
+                </div>
+                    
+                   
 
                 <div className="block">
                     <img src={Headshot} className="w-1/5 float-right" />
